770d001999ca2a87bb69ad0324be0e545ef54e8d
Arvind Kumar <3kumararvind@gmail.com>
2020-09-09 15:49:20 +0530

    Add files via upload

---
+++ js_modules/constant-values.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+//constant object for class mapping
+let academicClass={
+                  0:"PRE-PRIMARY",
+                  1:"ONE",
+                  2:"TWO",
+                  3:"THREE",
+                  4:"FOUR",
+                  5:"FIVE",
+                  6:"SIX",
+                  7:"SEVEN",
+                  8:"EIGHT",
+                  9:"NINE",
+                  10:"TEN",
+                  11:"ELEVEN",
+                  12:"TWELVE",
+                  13:"POLYTECHNIC/DIPLOMA",
+                  14:"B.A.",
+                  15:"B.COM.",
+                  16:"B.SC.",
+                  17:"M.A.",
+                  18:"M.COM.",
+                  19:"M.SC.",
+                  20:"B.TECH.",
+                  21:"M.TECH."
+              }
+module.exports={
+  academicClass:academicClass
+}
